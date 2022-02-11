3241a275268276163317eda4db1bccd03a253cf2
Oumaya <97735158+oumayargayg@users.noreply.github.com>
2022-02-11 10:47:18 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Fullname from './profile/Fullname';
+import ProfilePhoto from './profile/ProfilePhoto';
+
+function App() {
+  return (
+    <div className="App">
+      <ProfilePhoto/>
+      <Fullname/>
+    </div>
+  );
+}
+
+export default App;
